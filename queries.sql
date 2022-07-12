e2b62df890715838cfae1e22fdb698c0c7470ebc
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-07-12 22:58:17 +0300

    Edit query based on code review feedback

--- queries.sql
+++ queries.sql
@@ -3,7 +3,7 @@
 SELECT * from animals WHERE name = 'Luna';
 select * from animals where name like '%mon%';
 select name from animals where date_of_birth between '2016-01-01' and '2019-01-01';
-select * from animals where neutered ='1' and escape_attempts >3;
+select * from animals where neutered ='1' and escape_attempts < 3;
 select date_of_birth from animals where name like 'Agumon' or name like 'Pikachu';
 select name, escape_attempts from animals where weight_kg > 10.5;
  select name from animals where neutered = '1';
