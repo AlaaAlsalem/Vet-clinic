09647b484a93694f1d6af4003d25afd6059f91d8
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-07-12 21:44:08 +0300

    Add query for inserting data into animals table

--- data.sql
+++ data.sql
@@ -3,3 +3,7 @@
 INSERT INTO animals (name) VALUES ('Luna');
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
+insert into animals(name,date_of_birth, neutered, weight_kg) VALUES ('Agumon', '2022-02-03','1' , 10.23);
+insert into animals(name,date_of_birth,escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '2018-11-15',2,'1' , 8);
+ insert into animals(name,date_of_birth,escape_attempts, neutered, weight_kg) VALUES ('Pikachu', '2021-01-07',1,'1' , 15.04);
+ insert into animals(name,date_of_birth,escape_attempts, neutered, weight_kg) VALUES ('Devimon', '20170-05-12',5,'1' , 11);
