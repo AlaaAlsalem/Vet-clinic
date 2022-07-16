0a8899260795c1e09756f0308fe57bf61b621486
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-07-17 00:21:28 +0300

    Add query to create table for relation between species and vets

--- schema.sql
+++ schema.sql
@@ -42,3 +42,11 @@ CREATE TABLE vets(
     age   INT,
     date_of_graduation DATE
 );
+
+CREATE TABLE specializations(
+    species_id  INT NOT NULL,
+    vet_id      INT NOT NULL,
+    FOREIGN KEY (species_id) REFERENCES species(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (vet_id) REFERENCES vets(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    PRIMARY KEY (species_id, vet_id)
+);
